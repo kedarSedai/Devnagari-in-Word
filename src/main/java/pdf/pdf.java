94f0af8242c7bf7a6fcd0a93ffe22c25aabae0f2
kedar sedai <kedar.sedai05@gmail.com>
2021-02-04 10:15:31 +0545

    added seperate code for the pdf and word

--- src/main/java/pdf/pdf.java
+++ src/main/java/pdf/pdf.java
@@ -1,11 +1,13 @@
 package pdf;
 
+//Need to import itextpdf for pdf file
 import com.itextpdf.text.*;
 import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
+//Need to import spire libraries file for the word file
 import com.spire.doc.Document;
 import com.spire.doc.FileFormat;
 import com.spire.doc.Section;
@@ -16,6 +18,8 @@ import com.spire.doc.documents.ParagraphStyle;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+
+import static sun.plugin.javascript.navig.JSType.Document;
 
 /**
  * Created by kedar on 2/3/2021.
@@ -32,16 +36,33 @@ public class pdf {
     }
 
     public void createPdf(String Nepali) throws IOException, DocumentException {
+
+        //For the PDF file
+//        Document document = new Document();
+//        try
+//        {
+//            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(Hello.pdf));
+//            document.open();
+//            document.add(new Paragraph("केदार केदार");
+//            document.close();
+//            writer.close();
+//        } catch (DocumentException e)
+//        {
+//            e.printStackTrace();
+//        } catch (FileNotFoundException e)
+//        {
+//            e.printStackTrace();
+//        }
+//    }
+
+
+        //For the Document file.
         Document document = new Document();
 
         Section section = document.addSection();
-//        PdfWriter.getInstance(document, new FileOutputStream(Nepali));
-//        document.open();
-//
+
         Paragraph p2 = section.addParagraph();
         p2.appendText("केदार केदार");
-//        document.add(p2);
-//        document.close();
 
         // Save the document
         document.saveToFile("results/fonts/Nepalii.docx", FileFormat.Docx);
